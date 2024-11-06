4d5fae95be577985ebd5221c62b1a9f829106e23
lev Cedilnik <lev.cedilnik@preset.si>
2024-11-06 21:26:28 +0100

    https support

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+/*import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
@@ -11,5 +11,31 @@ export default defineConfig({
       },
     },
     host: '0.0.0.0', // makes the server accessible on the local network
+  },
+});
+*/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "cert/key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "cert/cert.pem")),
+    },
+    proxy: {
+      "/api": {
+        //target: "https://45.76.80.83:3000",
+        target: "https://45.76.80.83:3000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+    host: true, // Expose the server to your local network (optional)
   },
 });
